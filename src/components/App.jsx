9fe1c5c7ebc880d56044d6396d0bb7165073ba34
UserVasyk <106909139+UserVasyk@users.noreply.github.com>
2023-01-31 13:57:01 +0200

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,65 @@
-import React, { Component } from 'react';
-import Statistics from './Statistics/Statistics';
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    return Object.values([good, neutral, bad]).reduce(
+      (acc, value) => (acc += value),
+      0
+    );
   };
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => (acc += value), 0);
-  };
-  onLeaveFeedback = event => {
+  const onLeaveFeedback = event => {
     const value = event.target.name;
-    this.setState(prevState => {
-      return { [value]: prevState[value] + 1 };
-    });
+
+    switch (value) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        return;
+    }
   };
-  countPositiveFeedbackPercentage = () => {
-    if (this.countTotalFeedback() === 0) {
+  const countPositiveFeedbackPercentage = () => {
+    if (countTotalFeedback() === 0) {
       return 0;
     }
-    return Math.floor((this.state.good / this.countTotalFeedback()) * 100);
+    return Math.floor((good / countTotalFeedback()) * 100);
   };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const {
-      onLeaveFeedback,
-      countTotalFeedback,
-      countPositiveFeedbackPercentage,
-    } = this;
-    return (
-      <>
-        <Section title="Please Leave Feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={onLeaveFeedback}
+
+  return (
+    <>
+      <Section title="Please Leave Feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <Notification title="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() === 0 ? (
-            <Notification title="There is no feedback" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={countTotalFeedback}
-              positivePercentage={countPositiveFeedbackPercentage}
-            />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+        )}
+      </Section>
+    </>
+  );
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ListStats } from './Statistics.styled';
-export default class Statistics extends Component {
-  render() {
-    const { good, neutral, bad } = this.props;
-    return (
-      <div>
-        <ListStats>
-          <li>Good: {good}</li>
-          <li>Neutral: {neutral}</li>
-          <li>Bad: {bad}</li>
-          <li>Total: {this.props.total()}</li>
-          <li>Posiitive Feedback {this.props.positivePercentage()}%</li>
-        </ListStats>
-      </div>
-    );
-  }
-}
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  positivePercentage,
+  total,
+}) => {
+  return (
+    <div>
+      <ListStats>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total()}</li>
+        <li>Posiitive Feedback {positivePercentage()}%</li>
+      </ListStats>
+    </div>
+  );
+};
+
 Statistics.propTypes = {
   total: PropTypes.func.isRequired,
   positivePercentage: PropTypes.func.isRequired,
